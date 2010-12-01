62f0bd76f3a360ebe6a7069808e78a71fceba460
Dirk Kelly <dk@dirkkelly.com>
2010-12-01 14:16:41 +0800

    dependent destroy on product packings

--- lib/shop_packages/models/shop_packageable.rb
+++ lib/shop_packages/models/shop_packageable.rb
@@ -4,7 +4,7 @@ module ShopPackages
       
       def self.included(base)
         base.class_eval do
-          has_many :packings, :class_name => 'ShopPacking', :foreign_key => :product_id
+          has_many :packings, :class_name => 'ShopPacking', :foreign_key => :product_id, :dependent => :destroy
           has_many :packages, :class_name => 'ShopPackage', :foreign_key => :package_id, :through => :packings, :source => :package
           has_many :related,  :class_name => 'ShopProduct', :through     => :packings,   :source  => :product,  :uniq   => true
         end
