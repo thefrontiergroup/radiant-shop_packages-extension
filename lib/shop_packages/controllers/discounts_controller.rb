ee734399695c8a0bbbdf7afc9a4a57951dfebb4b
John Barker <jebarker@gmail.com>
2011-09-29 11:52:23 +0800

    Remove package buttons from discount editing screen

--- lib/shop_packages/controllers/discounts_controller.rb
+++ lib/shop_packages/controllers/discounts_controller.rb
@@ -8,9 +8,10 @@ module ShopPackages
           def config_edit_with_packages
             config_edit_without_packages
             
-            @buttons  << 'browse_packages'
-            @parts    << 'packages'
-            @popups   << 'browse_packages'
+            # XXX: Packages are no longer available to be edited or assigned to discounts
+            #@buttons  << 'browse_packages'
+            #@parts    << 'packages'
+            #@popups   << 'browse_packages'
           end
           alias_method_chain :config_edit, :packages
           
